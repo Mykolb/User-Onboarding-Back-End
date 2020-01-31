cdd420b443e70b2ef7b43c06fe385b0b26e68ed2
Mykol Benning <mykolbenning@MBP13inhMid2012.fios-router.home>
2020-01-31 16:09:14 -0500

    restructuring files

--- api/index.js
+++ api/index.js
@@ -7,6 +7,12 @@ require('dotenv').config();
 //import routers
 const student_info_router = require('../routes/student_info/student_info_router')
 
+
+const port = process.env.PORT || 5000
+server.listen(port, () => {
+    console.log(`*** Server is listening on port ${port}***\n`)
+})
+
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true})
 
